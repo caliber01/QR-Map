b178ac20a017713c20ff487f74e76b46ad977b1a
caliber01 <caliber1997@gmail.com>
2014-12-04 20:39:33 +0200

    improved search + some visual improvements

--- QR-Map/src/com/example/qr_map/Activities/MainActivity.java
+++ QR-Map/src/com/example/qr_map/Activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.qr_map.Activities;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,6 +13,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -57,6 +60,8 @@ public class MainActivity extends ActionBarActivity {
         mDrawerLayout = (DrawerLayout) this.findViewById(R.id.drawer_layout);
         mSideLinearLayout = (LinearLayout) this.findViewById(R.id.linear_drawer);
         
+        
+        
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,                 
                 mDrawerLayout,        
@@ -92,6 +97,15 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+     // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+               (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        
         return true;
     }
 
